935816aa5c6b356134488081c7212772595e788b
unknown <ljl@aqaeqe.com>
2025-07-11 11:30:33 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.remove((Integer)1);
+        System.out.println(list);
+
+
+    }
+}
